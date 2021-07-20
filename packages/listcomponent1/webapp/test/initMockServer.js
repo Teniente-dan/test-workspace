597506a27f30b1859f0bba9705ebf7d9a5fe8008
Teniente-dan <augustogp28@gmail.com>
2021-07-20 18:47:41 -0500

    rebase

--- packages/listcomponent1/webapp/test/initMockServer.js
+++ packages/listcomponent1/webapp/test/initMockServer.js
@@ -13,6 +13,7 @@ sap.ui.define([
         MessageBox.error(oError.message);
     }).finally(function () {
         // initialize the embedded component on the HTML page
+        console.log("requiere");
         sap.ui.require(["sap/ui/core/ComponentSupport"]);
     });
 });
